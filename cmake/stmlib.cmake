3d64e9f02d78ec73af1e07f69e207d07de199685
Martin Ribelotta <martinribelotta@gmail.com>
2021-04-09 21:20:39 -0300

    C++ and build changes
     - Add proper c++ support
     - Enable configure --size
     - Fix release build
     - Set C std to C99 and C++ std to C++11
     - Generate bin and hex files
     - Add target to burn in QSPI
    
    Signed-off-by: Martin Ribelotta <martinribelotta@gmail.com>

--- cmake/stmlib.cmake
+++ cmake/stmlib.cmake
@@ -17,6 +17,25 @@ target_sources(${PROJECT_NAME} PUBLIC ${HAL_SRC})
 target_sources(${PROJECT_NAME} PUBLIC ${STMLIB_PATH}/startup_stm32h750xx.s)
 set_target_properties(${PROJECT_NAME} PROPERTIES LINK_DEPENDS ${LINKER_SCRIPT})
 target_link_options(${PROJECT_NAME} PUBLIC "-T${LINKER_SCRIPT}")
-target_link_options(${PROJECT_NAME} PUBLIC "-Wl,--gc-sections")
-target_link_options(${PROJECT_NAME} PUBLIC "-Wl,--print-memory-usage")
 target_link_options(${PROJECT_NAME} PUBLIC "-nostartfiles")
+target_compile_features(${PROJECT_NAME} PUBLIC cxx_noexcept)
+target_compile_features(${PROJECT_NAME} PUBLIC cxx_std_11)
+target_compile_features(${PROJECT_NAME} PUBLIC c_std_99)
+
+set(HEX_FILE ${CMAKE_CURRENT_BINARY_DIR}/${PROJECT_NAME}.hex)
+set(BIN_FILE ${CMAKE_CURRENT_BINARY_DIR}/${PROJECT_NAME}.bin)
+
+add_custom_command(TARGET ${PROJECT_NAME} POST_BUILD
+    COMMAND ${CROSS_OBJCOPY} "-Oihex" $<TARGET_FILE:${PROJECT_NAME}> ${HEX_FILE})
+
+add_custom_command(TARGET ${PROJECT_NAME} POST_BUILD
+    COMMAND ${CROSS_OBJCOPY} "-Obinary" $<TARGET_FILE:${PROJECT_NAME}> ${BIN_FILE})
+
+set(BURN_ADDR 0x00000000)
+
+add_custom_target(burn-${PROJECT_NAME}
+                  COMMAND make RESET_AT_END=yes BURN_ADDR=${BURN_ADDR} BURN_FILE=${BIN_FILE} burn
+                  WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/qspi-loader
+                  DEPENDS ${PROJECT_NAME}
+                  COMMENT "Burning ${PROJECT_NAME}.bin at ${BURN_ADDR}"
+                  USES_TERMINAL)
