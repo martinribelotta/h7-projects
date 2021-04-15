4a3a13745245a3411e0ce015f205324aa339f461
Martin Ribelotta <martinribelotta@gmail.com>
2021-04-14 22:15:46 -0300

    Include headers in the cmake project
    Signed-off-by: Martin Ribelotta <martinribelotta@gmail.com>

--- cmake/stmlib.cmake
+++ cmake/stmlib.cmake
@@ -2,18 +2,25 @@ if(NOT DEFINED STMLIB_PATH)
     message(FATAL_ERROR "Undefined path for STMLIB_PATH")
 endif()
 
+list(APPEND _INCPATH "${STMLIB_PATH}/cmsis_core/Include/")
+list(APPEND _INCPATH "${STMLIB_PATH}/cmsis_device_h7/Include/")
+list(APPEND _INCPATH "${STMLIB_PATH}/stm32h7xx_hal_driver/Inc/")
+list(APPEND _INCPATH "${STMLIB_PATH}/stm32h7xx_hal_driver/Inc/Legacy/")
+
 target_compile_definitions(${PROJECT_NAME} PUBLIC -DUSE_FULL_LL_DRIVER -DUSE_HAL_DRIVER -DSTM32H750xx)
-target_include_directories(${PROJECT_NAME} PUBLIC ${STMLIB_PATH}/cmsis_core/Include/)
-target_include_directories(${PROJECT_NAME} PUBLIC ${STMLIB_PATH}/cmsis_device_h7/Include/)
-target_include_directories(${PROJECT_NAME} PUBLIC ${STMLIB_PATH}/stm32h7xx_hal_driver/Inc/)
-target_include_directories(${PROJECT_NAME} PUBLIC ${STMLIB_PATH}/stm32h7xx_hal_driver/Inc/Legacy/)
 
 file(GLOB HAL_SRC ${STMLIB_PATH}/stm32h7xx_hal_driver/Src/*.c)
-list(FILTER HAL_SRC EXCLUDE REGEX ".*_template.c")
 
+foreach(p ${_INCPATH})
+    target_include_directories(${PROJECT_NAME} PUBLIC ${p})
+    file(GLOB H_FILES ${p}/*.h)
+    list(APPEND HAL_H "${H_FILES}")
+endforeach()
+
+list(FILTER HAL_SRC EXCLUDE REGEX ".*_template.c")
 set(LINKER_SCRIPT ${STMLIB_PATH}/STM32H750VBTx_QSPI.ld)
 
-target_sources(${PROJECT_NAME} PUBLIC ${HAL_SRC})
+target_sources(${PROJECT_NAME} PUBLIC ${HAL_SRC} ${HAL_H})
 target_sources(${PROJECT_NAME} PUBLIC ${STMLIB_PATH}/startup_stm32h750xx.s)
 set_target_properties(${PROJECT_NAME} PROPERTIES LINK_DEPENDS ${LINKER_SCRIPT})
 target_link_options(${PROJECT_NAME} PUBLIC "-T${LINKER_SCRIPT}")
